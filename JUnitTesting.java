95d6f393f2c5ca5b17142d992892a39fb734c65a
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-28 02:42:24 +0530

    Square Root

--- JUnitTesting.java
+++ JUnitTesting.java
@@ -3,20 +3,28 @@ package co.bridgelabz.JUnitTesting;
 import java.util.Scanner;
 public class JUnitTesting 
 {
-	public static void main(String[] args) 
-	{
-		double p, r, payement;
-		int t;
-		Scanner sc = new Scanner(System.in); 
-	   	System.out.println("Enter Principal: ");
-	   	p = sc.nextDouble(); 
-	   	System.out.println("Enter Rate of Interest: ");
-	   	r = sc.nextDouble(); 
-	   	r = (r / 100) / 12; 
-	   	System.out.println("Enter Time period in years: ");
-	   	t = sc.nextInt();
-	   	t = t * 12; 
-	   	payement= (p * r) / (1 - Math.pow(1 + r, -t));
-	   	System.out.println("Payment: " + payement);   
-	 }
-	}
+	public static double squareRoot(double c, double l){
+        double t = c;
+        double root;                 
+        int count = 0;
+       while (true)
+        {
+            count++;
+            root = 0.5 * (t + (c / t));
+            if (Math.abs(root - t) < l)
+                break;
+            t = root;
+        }
+      return root;
+}
+	public static void main (String[] args)
+    {
+		Scanner sc = new Scanner(System.in);
+        double c;
+        double l = 0.00001;
+        System.out.println("Enter the number:");
+        c=sc.nextDouble();
+     
+        System.out.println(squareRoot(c, l));
+    }
+}
